158000d5050a7c5553dbcf84ab58b262e88ae8d9
jakubjanoska <104392165+jakubjanoska@users.noreply.github.com>
2023-02-28 20:33:53 +0100

    create of support tables, crossjoin of main table, some updates needed

--- blanket.sql
+++ blanket.sql
@@ -51,6 +51,77 @@ ORDER BY cpc.name, cp.date_from;
 
 
 
+CREATE OR REPLACE TABLE t_test
+	SELECT 
+		t.city, t.job, t.`size`, tt.food, tt.animal  
+	FROM t_22 AS t 
+	CROSS JOIN t_table_21 AS tt 
+	WHERE 
+		t.`size` IS NOT NULL 
+		
+/*
+ * summary table nr. one 
+ */		
+CREATE OR REPLACE TABLE t_cz_payroll_cut		
+SELECT 
+	cp.id,
+	cp.value,
+	cpu.name AS KC_or_thousand_of_ppl,
+	cpc.name AS typ_prepoctu,
+	cpib.name AS name_of_industry_branch,
+	cp.payroll_quarter AS `quarter`,
+	cp.payroll_year AS `year`,
+	concat(cp.payroll_quarter, '.', cp.payroll_year) AS quarter_year
+FROM czechia_payroll AS cp 
+JOIN czechia_payroll_value_type AS cpvt 
+	ON cp.value_type_code = cpvt.code 
+JOIN czechia_payroll_unit AS cpu 
+	ON cp.unit_code = cpu.code 
+JOIN czechia_payroll_calculation AS cpc 
+	ON cp.calculation_code = cpc.code 
+JOIN czechia_payroll_industry_branch AS cpib 
+	ON cp.industry_branch_code = cpib.code
+WHERE value IS NOT NULL;
+
+CREATE OR REPLACE TABLE t_cz_price_cut
+SELECT
+    cp.id,
+	ROUND(AVG(cp.value),2) AS avg_price,
+    cpc.name AS product,
+	year(cp.date_from) AS `year`,
+    quarter(cp.date_from) AS quartal,
+    cr.name AS region,
+    concat(quarter(cp.date_from), '.' , year(cp.date_from)) AS quarter_year
+FROM
+    czechia_price AS cp
+JOIN czechia_price_category AS cpc 
+    ON cp.category_code = cpc.code 
+JOIN czechia_region AS cr  
+    ON cp.region_code = cr.code 
+GROUP BY cpc.name, YEAR(cp.date_from), quarter(cp.date_from), cr.name  
+ORDER BY cpc.name, cp.date_from;
+
+SELECT * 
+FROM t_cz_payroll_cut AS cprl 
+CROSS JOIN t_cz_price_cut  AS cpc 
+WHERE cprl.quarter_year = cpc.quarter_year
+ORDER BY cprl.name_of_industry_branch, cprl.typ_prepoctu
+
+SELECT cprl.value, cprl.typ_prepoctu, cprl.name_of_industry_branch, cprl.quarter_year,
+	cpc.quarter_year, cpc.avg_price, cpc.product, cpc.region  
+FROM t_cz_payroll_cut AS cprl 
+CROSS JOIN t_cz_price_cut  AS cpc 
+WHERE cprl.quarter_year = cpc.quarter_year
+ORDER BY cprl.name_of_industry_branch, cprl.typ_prepoctu
+
+/*
+ * End of table1
+ */		
+		
+SELECT 
+*,
+concat(tcpc.quarter, '.', tcpc.`year`) AS quarter_year
+FROM t_cz_payroll_cut AS tcpc 
 
 
 SELECT 
